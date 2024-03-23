583e6f354923ba005262c1fa8c67529c2c89c600
hassankyanzi <kyanzi.hassan2@gmail.com>
2024-03-23 14:48:55 +0300

    I made changes to include Werkzeug 2.1.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 #Install flask / Version must be 2.1.0
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'Install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+   unless => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  unless => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
+
 }
