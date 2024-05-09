ed3bc4f003081cca388d0567042a22451ae39a63
hassankyanzi <kyanzi.hassan2@gmail.com>
2024-05-09 09:36:08 +0300

    I made some modifications to the 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,6 @@ Script that queries subscribers on a given Reddit subreddit.
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
@@ -13,7 +12,15 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        if 'data' in data and 'subscribers' in data['data']:
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
     else:
         return 0
+
+# For testing
+if __name__ == '__main__':
+    subreddit = input("Enter subreddit name: ")
+    print(number_of_subscribers(subreddit))
